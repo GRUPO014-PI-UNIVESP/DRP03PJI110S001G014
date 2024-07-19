66786a2e75d3cc5242f3e60b1dc2e62d0da567f4
GRUPO014-PI-UNIVESP <1829240@aluno.univesp.br>
2024-07-18 21:32:51 -0300

    organização da pasta

---
+++ DRP03PJI240S001G015/01.PHP/index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+  <form action="" method="POST">
+        <div class="LoginBox">
+            <fieldset>
+                <legend style="font-size: 25px;"><b>Login</b></legend>
+                <br><br>
+                <div class="inputBox">
+                    <input class="inputUser" type="text" name="usuario" id="usuario" required>
+                    <label class="labelInput" for="usuario" >Digite seu ID de Usuário</label>                    
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input class="inputUser" type="password" name="senha" id="senha" required>
+                    <label class="labelInput" for="senha" >Digite sua Senha (6 dígitos)</label>                    
+                </div>
+                <br><br><br>
+                <div>
+                    <input type="submit" name="submit" id="submit">
+                </div>
+                <br>
+            </fieldset>
+            <br>
+            <p style="font-size:9px; color:bisque;text-align: center;">Developed by EdsonmmInfo 2024</p>
+        </div>
+    </form>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
